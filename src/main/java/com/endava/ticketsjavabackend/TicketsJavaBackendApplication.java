5683f4096c9f49bf430a32a5c80879ab4c8a8cde
Cristi Tomoiaga <Cristi-Tomoiaga@users.noreply.github.com>
2023-07-19 12:49:58 +0300

    Initial project structure

---
+++ src/main/java/com/endava/ticketsjavabackend/TicketsJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.endava.ticketsjavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsJavaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsJavaBackendApplication.class, args);
+    }
+
+}
