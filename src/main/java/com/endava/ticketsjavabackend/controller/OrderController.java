7ec331a8dfb30ff8755f5002436dc7122eb81f39
Cristi Tomoiaga <Cristi-Tomoiaga@users.noreply.github.com>
2023-07-21 15:50:55 +0300

    Implement order endpoints

---
+++ src/main/java/com/endava/ticketsjavabackend/controller/OrderController.java
@@ -0,0 +1,41 @@
+package com.endava.ticketsjavabackend.controller;
+
+import com.endava.ticketsjavabackend.mapstruct.dto.OrderGetDto;
+import com.endava.ticketsjavabackend.mapstruct.dto.OrderPostDto;
+import com.endava.ticketsjavabackend.service.OrderServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    private static final Integer customerId = 3;
+    private final OrderServiceImpl orderServiceImpl;
+
+    public OrderController(OrderServiceImpl orderServiceImpl) {
+        this.orderServiceImpl = orderServiceImpl;
+    }
+
+    @GetMapping
+    public List<OrderGetDto> getAllByCustomerId() {
+        return orderServiceImpl.findOrdersByCustomerId(customerId);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveOrder(@RequestBody OrderPostDto order) {
+        OrderGetDto result = orderServiceImpl.saveOrder(order, customerId);
+
+        if (result == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Map<String, OrderGetDto> resultBody = new HashMap<>();
+        resultBody.put("order", result);
+        return new ResponseEntity<>(resultBody, HttpStatus.CREATED);
+    }
+}
